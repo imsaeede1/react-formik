4422f98587b347577f9617fc9685156b9da4b620
saeede <zahramaghsudi@gmail.com>
2021-10-21 15:41:15 +0330

    added h1

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -3,6 +3,7 @@ import Header from "../components/Header/Header";
 const Layout = ({ children }) => {
   return (
     <div>
+      <h1>React-App !!!</h1>
       <Header />
       {children}
     </div>
